625312b0d43e36afd89f9902c5cb5e09ee6087ae
Angell6991 <miguel9196.guzman@gmail.com>
2026-05-19 20:30:32 -0500

    modelues texting

---
+++ hypr_02/modules/texting.lua
@@ -0,0 +1,23 @@
+----------lansar una notificacion ---------
+hl.bind("SUPER + X", function()
+    hl.notification.create({
+        text        =   "Hola Mundo",
+        duration    =   5000,
+        color       =   "rgb(226,226,226)",
+        font_size   =   18,
+    })
+end)
+
+--------- notify new window open ----------
+hl.on("window.open", function(w)
+    local ws = hl.get_active_workspace()
+
+    hl.notification.create({
+        text        = "  WS: " .. ws.name .. " | NW: " .. w.title,
+        duration    = 5000,     -- 5segundos     
+        color       = "rgb(226,226,226)",
+        font_size   = 18,
+    })
+end)
+
+
