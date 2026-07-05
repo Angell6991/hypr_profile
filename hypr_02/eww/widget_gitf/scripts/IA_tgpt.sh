02cc8929d91a15c984eacbb7a3c31c32d20856d1
Angell6991 <miguel9196.guzman@gmail.com>
2026-07-05 14:08:12 -0500

    wi-fi

--- hypr_02/eww/widget_gitf/scripts/IA_tgpt.sh
+++ hypr_02/eww/widget_gitf/scripts/IA_tgpt.sh
@@ -3,7 +3,7 @@
 
 terminal="kitty"
 shell="$SHELL"
-execute="btop"
+execute="impala"
 # execute="touch /tmp/GPT.md & tgpt --provider pollinations -i --log /tmp/GPT.md"
 
 # $terminal -- $shell -c "$execute; exec $shell"
